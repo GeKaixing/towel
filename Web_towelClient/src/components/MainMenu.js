10805a25e491238b7f7a78422188f894c6f5c5a8
GeKaixing <2890901420@qq.com>
2024-06-28 01:18:29 +0800

    css(web_towel):添加样式

--- Web_towelClient/src/components/MainMenu.js
+++ Web_towelClient/src/components/MainMenu.js
@@ -1,12 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import style from './MainMenu.module.css'
-import { HomeFilled, MessageFilled, PlusSquareFilled, BulbFilled } from '@ant-design/icons';
 import { noReadNumbers } from '../context/noReadNumbers';
 export default function MainMenu() {
   const [width, setwidth] = useState(window.innerWidth)
   const [localStorageData, setLocalStorageData] = useState({})
-  const {noReadNumber}=useContext(noReadNumbers)
+  const { noReadNumber } = useContext(noReadNumbers)
+  const router = useLocation()
   useEffect(() => {
     if (localStorage.getItem('loginData')) {
       setLocalStorageData(JSON.parse(localStorage.getItem('loginData')))
@@ -20,7 +20,37 @@ export default function MainMenu() {
       // 卸载警听
       window.removeEventListener('resize', handlerResize);
     }
+
   }, [window.innerWidth])
+
+  class IconPath {
+    constructor(path) {
+      this.path = process.env.PUBLIC_URL + path
+    }
+  }
+  class Howshowicon {
+    constructor(MainMenuIcon = '/static/MainMenuIcon/首页.svg', MainMenuIconPitchUp = '/static/MainMenuIconPitchUp/首页.svg') {
+      this.MainMenuIcon = new IconPath(MainMenuIcon)
+      this.MainMenuIconPitchUp = new IconPath(MainMenuIconPitchUp)
+    }
+    howshowicon(path = '/') {
+      return (router.pathname === path) ? this.MainMenuIconPitchUp : this.MainMenuIcon
+    }
+    home() {
+      const pathname = router.pathname.split('/')
+      if (router.pathname === '/') {
+        return this.MainMenuIconPitchUp
+      } else if (pathname[1] === 'homepage') {
+        return this.MainMenuIconPitchUp
+      }else{
+        return this.MainMenuIcon
+      }
+    }
+  }
+  const Howshowicon1 = new Howshowicon()
+  const Howshowicon2 = new Howshowicon('/static/MainMenuIcon/提示.svg', '/static/MainMenuIconPitchUp/提示.svg')
+  const Howshowicon3 = new Howshowicon('/static/MainMenuIcon/评论.svg', '/static/MainMenuIconPitchUp/评论.svg')
+  const Howshowicon4 = new Howshowicon('/static/MainMenuIcon/添加.svg', '/static/MainMenuIconPitchUp/添加.svg')
   return (
     <div>
       <div className={style.bar} >
@@ -31,14 +61,14 @@ export default function MainMenu() {
               <img src={process.env.PUBLIC_URL + '/logo.png'} ></img>
             </div>
         }
-        <Link className={style.link} to="/"> <div><HomeFilled />{width < 750 ? null : "主页"}</div></Link>
-        <Link className={style.link} to="/about"><div><BulbFilled />{width < 750 ? null : "关于"}</div></Link>
+        <Link className={style.link} to="/"> <img src={Howshowicon1.home().path} ></img>{width < 750 ? null : "主页"}</Link>
+        <Link className={style.link} to="/about"><img src={Howshowicon2.howshowicon('/about').path}></img>{width < 750 ? null : "关于"}</Link>
         <Link className={style.link} to='/Message' >
           <div style={{ display: 'flex', flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-            <MessageFilled  />{width < 750 ? null : "消息"}
+            <img src={Howshowicon3.howshowicon('/Message').path}></img>{width < 750 ? null : "消息"}
             <div>{noReadNumber}</div>
           </div></Link>
-        <Link className={style.link} to='/post' ><div><PlusSquareFilled />{width < 750 ? null : "发帖"}</div></Link>
+        <Link className={style.link} to='/post' ><img src={Howshowicon4.howshowicon('/post').path}></img>{width < 750 ? null : "发帖"}</Link>
         {localStorageData.jwt ? null : <Link className={style.link} to="/login">{width < 750 ? null : "登录"}</Link>}
       </div>
       {/* 脱离文档流，父元素坍塌问题 */}
--- Web_towelClient/src/components/mainMenuComponents/addComponents/AddContent.js
+++ Web_towelClient/src/components/mainMenuComponents/addComponents/AddContent.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { LikeFilled, StarFilled, RocketFilled, MessageFilled } from '@ant-design/icons';
 import style from './AddContent.module.css';
 
 
@@ -9,6 +8,8 @@ export default function Portal() {
     const [textareaData, settextareaData] = useState('');
     const [showImageData, setShowImageData] = useState('');
     const [responseImageData, setResponseImageData] = useState('')
+    const [isHovered, setIsHovered] = useState(false);
+    const [isHoveredImage, setIsHoveredImage] = useState(false);
     const upImageApi = async (e) => {
         e.preventDefault();
         try {
@@ -66,6 +67,22 @@ export default function Portal() {
             });
         } catch (error) { console.log(error) }
     }
+
+    class PostIcon {
+        constructor(path) {
+            {/* global process*/ }
+            this.path = process.env.PUBLIC_URL + path
+        }
+    }
+    const postIcon1 = new PostIcon('/static/postIcon/赞.svg')
+    const postIcon2 = new PostIcon('/static/postIcon/评论.svg')
+    const postIcon3 = new PostIcon('/static/postIcon/星星.svg')
+    const postIcon4 = new PostIcon('/static/postIcon/分享.svg')
+    const postIcon5 = new PostIcon('/static/otherIcon/图片添加.svg')
+    const postIcon55 = new PostIcon('/static/otherIconPitchUp/图片添加.svg')
+    const postIcon6 = new PostIcon('/static/MainMenuIcon/添加.svg')
+    const postIcon7 = new PostIcon('/static/MainMenuIconPitchUp/添加.svg')
+
     return (
         <div className={style.addContent}>
             <div className={style.messagebigbox}>
@@ -84,10 +101,10 @@ export default function Portal() {
                         />
                     </div>
                     <div className={style.thisshowbottom}>
-                        <div id='like' className={style.whitelike}><LikeFilled />{'likes'}</div>
-                        <div className={style.MessageFilled}><MessageFilled />{'comments'}</div>
-                        <div className={style.star}><StarFilled />收藏{'favorites'}</div>
-                        <div className={style.share}><RocketFilled />分享</div>
+                        <img style={{ width: '100%', height: '100%', verticalAlign: 'middle', textAlign: 'center' }} src={postIcon1.path}></img>
+                        <img style={{ width: '100%', height: '100%', verticalAlign: 'middle', textAlign: 'center' }} src={postIcon2.path}></img>
+                        <img style={{ width: '100%', height: '100%', verticalAlign: 'middle', textAlign: 'center' }} src={postIcon3.path}></img>
+                        <img style={{ width: '100%', height: '100%', verticalAlign: 'middle', textAlign: 'center' }} src={postIcon4.path}></img>
                     </div>
                 </div>
             </div>
@@ -98,9 +115,27 @@ export default function Portal() {
                         <div className={style.inputFlie} onClick={() => setShowImageData('')}>删除图片</div>
                     </>
                 )}
-                <label htmlFor='inputfile' className={style.inputFlie} title='添加图片'>上传图片</label>
+                <label htmlFor='inputfile' className={style.inputFlie} title='添加图片'
+                    onMouseEnter={() => setIsHoveredImage(true)}
+                    onMouseLeave={() => setIsHoveredImage(false)}
+                >
+                    {isHoveredImage ?
+                        <img style={{ width: '100%', height: '100%', verticalAlign: 'middle', textAlign: 'center' }} src={postIcon55.path} alt='添加图片'></img>
+                        :
+                        <img style={{ width: '100%', height: '100%', verticalAlign: 'middle', textAlign: 'center' }} src={postIcon5.path} alt='添加图片'></img>
+                    }
+                </label>
                 <input type="file" id='inputfile' style={{ display: 'none' }} onChange={upImageApi} />
-                <div className={style.addbuttonAdd} onClick={sendPostApi}>添加</div>
+                <div className={style.addbuttonAdd} onClick={sendPostApi}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}>
+                    {
+                        isHovered ?
+                            <img style={{ width: '100%', height: '100%', verticalAlign: 'middle', textAlign: 'center' }} src={postIcon7.path} alt='发布'></img>
+                            :
+                            <img style={{ width: '100%', height: '100%', verticalAlign: 'middle', textAlign: 'center' }} src={postIcon6.path} alt='发布'></img>
+                    }
+                </div>
             </div>
         </div>
     );
