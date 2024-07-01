911a38a881b692d1c492b53456a3cd998a027ce2
GeKaixing <2890901420@qq.com>
2024-07-01 13:55:51 +0800

    feat(Web_towel): 用户自己选择

--- Web_towelClient/src/components/mainMenuComponents/settingComponents/SettingBackgroundPage/SettingBackgroundImg.js
+++ Web_towelClient/src/components/mainMenuComponents/settingComponents/SettingBackgroundPage/SettingBackgroundImg.js
@@ -6,8 +6,22 @@ export default function SettingBackgroundImg() {
     const [imgData, setImgDate] = useState('')
     const { colorModel, setColorModel } = useContext(selectLightorDarkContext)
     const SettingBackgroundImgHandler = (e) => {
-        setImgDate(e.target.file[0])
-        console.log(imgData)
+        e.preventDefault();
+        const file = e.target.files[0];
+        if (file.size < 5000000) {
+            const fileReader = new FileReader();
+            fileReader.onload = (event) => {
+                const imageData = event.target.result;
+                setImgDate(imageData);
+                setColorModel(prevColorModel => !prevColorModel);
+                localStorage.setItem('backgroundimg', imageData)
+                localStorage.setItem('color-model', 'bing')
+                localStorage.setItem('background-model', 'diy')
+            };
+            fileReader.readAsDataURL(file);
+        } else {
+            alert('请使用小于5MB的图片')
+        }
     }
     const SettingBackgroundBingImgHandler = () => {
         axios({ url: 'http://127.0.0.1:4000/HPImageArchive' })
@@ -15,6 +29,7 @@ export default function SettingBackgroundImg() {
                 setImgDate('https://cn.bing.com' + response.data.images[0].url)
                 localStorage.setItem('backgroundimg', 'https://cn.bing.com' + response.data.images[0].url)
                 localStorage.setItem('color-model', 'bing')
+                localStorage.setItem('background-model', 'bing')
                 setColorModel(!colorModel)
             })
             .catch((error) => { console.log(error) })
@@ -34,9 +49,12 @@ export default function SettingBackgroundImg() {
     }, [imgData, colorModel])
     return (
         <div className={style.SettingBackgroundImg}>
-            <div onClick={SettingBackgroundBingImgHandler} className={style.SettingBackgroundImgBing}>使用bing每日壁纸</div>
+            <div onClick={SettingBackgroundBingImgHandler} className={style.SettingBackgroundImgBing}>使用bing每日壁纸
+                {localStorage.getItem('color-model') === 'bing' ? localStorage.getItem('background-model') === 'bing' ? <div className={style.dot}></div> : null : null}
+            </div>
             <form onSubmit={(e) => e.preventDefault()}>
-                <input type='file' onChange={SettingBackgroundImgHandler}></input>
+                <input type='file' onChange={SettingBackgroundImgHandler} accept="image/*"></input>
+                {localStorage.getItem('color-model') === 'bing' ? localStorage.getItem('background-model') === 'diy' ? <div className={style.dot}></div> : null : null}
             </form>
         </div>
     )
