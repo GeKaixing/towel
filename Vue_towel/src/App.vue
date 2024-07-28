b738f360c574e908f5e75b330993ba144e9e32d6
GeKaixing <2890901420@qq.com>
2024-07-28 11:05:07 +0800

    style(Vue_towel):修改mian大小

--- Vue_towel/src/App.vue
+++ Vue_towel/src/App.vue
@@ -1,23 +1,23 @@
 <script setup>
 import Head from './views/head/Head.vue'
-import Post from './views/body/post/Post.vue'
+/* import Post from './views/body/post/Post.vue'
 import postContent from './views/body/post/postContent.vue'
 import add from './views/body/add/add.vue'
-import user from './views/body/user/user.vue'
+import user from './views/body/user/user.vue' */
 
 import Foot from './views/foot/Foot.vue'
 </script>
 <template>
   <div class="flex pt-2  ">
-    <Head></Head>
+    <Head></Head> <!-- 20% -->
     <!-- <Post></Post> -->
     <!-- <postContent></postContent> -->
     <!-- <add></add> -->
     <!-- <user></user> -->
-     <div class="ml-[30%] w-[30%]">
+     <div class="ml-[30%] w-[40%]"><!-- 40% -->
       <RouterView></RouterView>
      </div>
-    <Foot></Foot>
+    <Foot></Foot><!-- 20% -->
   </div>
 </template>
 <style></style>
