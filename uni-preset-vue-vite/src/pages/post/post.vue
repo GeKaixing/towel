8c38de671fdd86cbd9c2604e33cc56df2e743339
GeKaixing <2890901420@qq.com>
2024-08-21 23:02:41 +0800

    rec(uniapp_towel):rec

---
+++ uni-preset-vue-vite/src/pages/post/post.vue
@@ -0,0 +1,109 @@
+<script setup>
+import pageLayout from '../../style/pagelayout.vue'
+import { onMounted ,ref} from "vue"
+const resData=ref([])
+onMounted(()=>{
+    uni.request({
+        url: 'http://127.0.0.1:4000/post',
+        header: {
+            'content-type': 'application/json' // 设置请求头
+        },
+        success: function (res) {
+            resData.value=res.data
+        },
+        fail: function (res) {
+            console.log(res)
+        }
+    })
+})
+</script>
+<template>
+    <pageLayout>
+        <view class="post" v-for="(item,index) in  resData" :key="index">
+            <view class="post-head">
+                <view class="post-image-name">
+                    <image :src="item.user.headimg"
+                        class="post-image"></image>
+                    <view class="post-name">{{ item.user.username }}</view>
+                </view>
+                <view class="post-head-button">...</view>
+            </view>
+            <view class="post-context">{{item.postText}}</view>
+            <view v-if="item?.postImages">{{ item.postImages }}</view>
+            <view class="post-button">
+                <view class="post-button-icon">
+                    <image src="../../static/postIcon/赞.svg" mode="scaleToFill"></image>
+                    <view>{{item.postLike}}</view>
+                </view>
+                <view  class="post-button-icon">
+                    <image src="../../static/postIcon/评论.svg" mode="scaleToFill"></image>
+                    <view>{{item.postComment}}</view>
+                </view>
+                <view  class="post-button-icon">
+                    <image src="../../static/postIcon/星星.svg" mode="scaleToFill"></image>
+                    <view>{{item.postFavorite}}</view>
+                </view>
+                <view  class="post-button-icon">
+                    <image src="../../static/postIcon/分享.svg" mode="scaleToFill"></image>
+                </view>
+            </view>
+        </view>
+    </pageLayout>
+</template>
+<style scoped>
+.post {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10rpx;
+    border-radius: 10rpx;
+    padding: 10rpx;
+    border: 4rpx solid #f6f6f6;
+
+}
+
+.post-head {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+}
+
+.post-image-name {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap: 10rpx;
+}
+
+.post-image {
+    width: 80rpx;
+    height: 80rpx;
+    border-radius: 100%;
+}
+
+.post-name {
+    font-weight: bold;
+}
+
+.post-head-button {
+    font-weight: bold;
+}
+
+.post-button {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
+
+.post-button>view>image {
+    width: 40rpx;
+    height: 40rpx;
+    border-radius: 100%;
+    object-fit: cover;
+}
+.post-button-icon{
+    display: flex;
+    align-items: center;
+}
+</style>
---
+++ uni-preset-vue-vite/src/pages/user/signup.vue
@@ -0,0 +1,81 @@
+<script setup>
+import pageLayout from '../../style/pagelayout.vue'
+import {ref} from 'vue'
+const emailData=ref('')
+const isShow=ref(false)
+/*显示验证码框 */
+const idShowHandler=()=>{
+    isShow.value=true
+}
+const navigateTo=()=>{
+    uni.navigateTo({url:"pages/user/login"})
+}
+</script>
+<template>
+    <pageLayout>
+        <view class="layout">
+        <image class="logo" src="https://raw.githubusercontent.com/GeKaixing/towel/main/README_static/logo.png"></image>
+        <view action="" class="login">
+            <input type="text" class="login-account" placeholder="account">
+            <input type="poassword" class="login-password" placeholder="password">
+            <input type="email" v-model="emailData" class="login-password" placeholder="email">
+            <input type="text" v-if="isShow" class="login-password" maxlength="6" placeholder="验证码">
+            <view v-if="emailData" class="login-submit" @click="idShowHandler">发送验证码</view>
+            <view class="login-submit"> 注册</view>
+            <view class="login-submit-login"  @click="navigateTo">登录</view>
+        </view>
+    </view>
+    </pageLayout>
+</template>
+<style scoped>
+.layout {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+}
+.logo {
+    position: relative;
+    left: 50%;
+    transform: translate(-50%);
+    width: 100rpx;
+    height: 100rpx;
+    object-fit: contain;
+}
+
+.login {
+    width: 500rpx;
+    display: flex;
+    flex-direction: column;
+    gap: 20rpx;
+}
+
+.login-account {
+    width: 100%;
+    height: 60rpx;
+    background-color: #f6f6f6;
+}
+
+.login-password {
+    width: 100%;
+    height: 60rpx;
+    background-color: #f6f6f6;
+}
+
+.login-submit {
+    display: flex;
+    width: 50%;
+    height: 60rpx;
+    align-self: center;
+    background-color: #f6f6f6;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+}
+.login-submit-login{
+
+    display: flex;
+    justify-content: center;
+}
+</style>
