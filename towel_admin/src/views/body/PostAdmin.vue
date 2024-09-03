9bc833723f9f5dc66ac9b1ff985247082ef476e3
GeKaixing <x2890901420@Gmail.com>
2024-09-03 22:34:05 +0800

    feat(towel_admin):添加复选框

--- towel_admin/src/views/body/PostAdmin.vue
+++ towel_admin/src/views/body/PostAdmin.vue
@@ -1,6 +1,6 @@
 <script setup>
 import axios from 'axios';
-import { onMounted, ref,watch} from 'vue';
+import { computed, onMounted, reactive, ref,watch} from 'vue';
 
 const columns = [
   {
@@ -45,18 +45,45 @@ watch(reload,()=>{
 })
 const deleteHandler=(dd)=>{
   axios.delete(`http://127.0.0.1:4000/delpostadmin/${dd._id}`)
-  .then(res => {reload.value=!reload.value })
+  .then(() => {reload.value=!reload.value })
   .catch(error => console.log(error))
 }
+
+const state = reactive({
+  selectedRowKey: [],
+  // Check here to configure the default column
+  loading: false,
+});
+const hasSelected = computed(() => state.selectedRowKey.length > 0);
+const start = () => {
+  state.loading = true;
+  // ajax request after empty completing
+  setTimeout(() => {
+    state.loading = false;
+    state.selectedRowKey = [];
+  }, 1000);
+};
 </script>
 <template>
+  <a-button type="primary" :disabled="!hasSelected"  :loading="state.loading" @click="start">清除选择</a-button>
   <a-table
     :dataSource="resData"
     :columns="columns"
     :pagination="{ pageSize: 50 }"
+    :rowKey="(record) => record._id"
     :scroll="{ y: 240 }"
+    :row-selection="{
+      selectedRowKeys: state.selectedRowKey,
+      type: 'radio', // 使用复选框
+      hideDefaultSelections: true,
+      onChange: (selectedRowKeys, selectedRows) => {
+        console.log('Selected Row Keys:', selectedRowKeys) // 调试日志
+        console.log('Selected Rows:', selectedRows) // 调试日志
+        state.selectedRowKey = selectedRowKeys
+      }
+      }"
   >
-    <template #bodyCell="{column, text, record }">
+    <template #bodyCell="{column, record }">
       <template v-if="column.dataIndex === 'action'">
            <a @click="deleteHandler(record)">删除</a>
       </template>
--- towel_admin/src/views/body/userAdmin.vue
+++ towel_admin/src/views/body/userAdmin.vue
@@ -1,46 +1,23 @@
 <script setup>
 import axios from 'axios'
-import { onMounted, ref, watch } from 'vue'
+import { computed, onMounted, reactive, ref } from 'vue'
+
 const columns = [
-  {
-    title: '姓名',
-    dataIndex: 'username'
-  },
-  {
-    title: '生日',
-    dataIndex: 'birthday'
-  },
-  {
-    title: '邮件',
-    dataIndex: 'email'
-  },
-  {
-    title: '电话',
-    dataIndex: 'phoneNumber'
-  },
-  {
-    title: '权限',
-    dataIndex: 'auth'
-  },
-  {
-    title: '状态',
-    dataIndex: 'state'
-  },
-  {
-    title: '操作',
-    dataIndex: 'action'
-  }
+  { title: '姓名', dataIndex: 'username' },
+  { title: '生日', dataIndex: 'birthday' },
+  { title: '邮件', dataIndex: 'email' },
+  { title: '电话', dataIndex: 'phoneNumber' },
+  { title: '权限', dataIndex: 'auth' },
+  { title: '状态', dataIndex: 'state' },
+  { title: '操作', dataIndex: 'action' }
 ]
+
 const resData = ref(null)
 const reload = ref(false)
 const reloadHandler = () => {
   reload.value = !reload.value
 }
-/* const deleteHandler = (dd) => {
-  axios.delete(`http://127.0.0.1:4000/deleteuser/${dd._id}`)
-  .then(res => { reloadHandler()})
-  .catch(error => console.log(error))
-} */
+
 const banHandler = (dd) => {
   axios
     .post(`http://127.0.0.1:4000/banuser/${dd._id}`)
@@ -49,6 +26,7 @@ const banHandler = (dd) => {
     })
     .catch((error) => console.log(error))
 }
+
 const offBanHandler = (dd) => {
   axios
     .post(`http://127.0.0.1:4000/offbanuser/${dd._id}`)
@@ -57,6 +35,7 @@ const offBanHandler = (dd) => {
     })
     .catch((error) => console.log(error))
 }
+
 const sealingHandler = (dd) => {
   axios
     .post(`http://127.0.0.1:4000/sealinguser/${dd._id}`)
@@ -65,6 +44,7 @@ const sealingHandler = (dd) => {
     })
     .catch((error) => console.log(error))
 }
+
 const offSealingHandler = (dd) => {
   axios
     .post(`http://127.0.0.1:4000/offsealinguser/${dd._id}`)
@@ -73,6 +53,7 @@ const offSealingHandler = (dd) => {
     })
     .catch((error) => console.log(error))
 }
+
 const getResDataApi = () => {
   axios
     .get('http://127.0.0.1:4000/alluser')
@@ -81,41 +62,66 @@ const getResDataApi = () => {
     })
     .catch((error) => console.log(error))
 }
+
 onMounted(() => {
   getResDataApi()
 })
-watch(resData, () => {
-  getResDataApi()
-})
-// const rowSelection = 
 
+const state = reactive({
+  selectedRowKey: [],
+  // Check here to configure the default column
+  loading: false,
+});
+const hasSelected = computed(() => state.selectedRowKey.length > 0);
+const start = () => {
+  state.loading = true;
+  // ajax request after empty completing
+  setTimeout(() => {
+    state.loading = false;
+    state.selectedRowKey = [];
+  }, 1000);
+};
 </script>
+
 <template>
+
+<a-button type="primary" :disabled="!hasSelected"  :loading="state.loading" @click="start">清除选择</a-button>
   <a-table
     :dataSource="resData"
     :columns="columns"
     :pagination="{ pageSize: 50 }"
     :scroll="{ y: 240 }"
+    :rowKey="(record) => record._id"
+    :row-selection="{
+      selectedRowKeys: state.selectedRowKey,
+      type: 'radio', // 使用复选框
+      hideDefaultSelections: true,
+      onChange: (selectedRowKeys, selectedRows) => {
+        console.log('Selected Row Keys:', selectedRowKeys) // 调试日志
+        console.log('Selected Rows:', selectedRows) // 调试日志
+        state.selectedRowKey = selectedRowKeys
+      }
+    }"
   >
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'state'">
-        <!-- <a @click="deleteHandler(record)">删除</a> -->
         <div class="flex space-x-2">
           <div v-if="record.ban">禁言</div>
           <div v-else>-</div>
           <div v-if="record.sealing">封禁</div>
         </div>
       </template>
+
       <template v-if="column.dataIndex === 'action'">
-        <!-- <a @click="deleteHandler(record)">删除</a> -->
         <div class="flex space-x-2">
           <a v-if="!record.ban" @click="banHandler(record)">禁言</a>
           <a v-else @click="offBanHandler(record)">取消禁言</a>
-          <a v-if="!record.sealing" @click="sealingHandler(record)"> 封禁</a>
-          <a v-else @click="offSealingHandler(record)">取消禁言</a>
+          <a v-if="!record.sealing" @click="sealingHandler(record)">封禁</a>
+          <a v-else @click="offSealingHandler(record)">取消封禁</a>
         </div>
       </template>
     </template>
   </a-table>
 </template>
+
 <style scoped></style>
