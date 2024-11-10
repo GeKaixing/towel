4411d693de28615727a3f2a73ed8694f92b8e985
GeKaixing <2890901420@qq.com>
2024-11-10 18:19:47 +0800

    fix(towel-Server):修复bug

--- towelSever/index.js
+++ towelSever/index.js
@@ -1,30 +1,24 @@
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
 import http from 'http'
-import multer from 'multer'
-import fs from 'fs'
 import {Server} from 'socket.io'
 import socketio from './src/socket/index.js'
 import authMiddleware from './src/routers/auth/index.js'
-import commonRoute from './src/routers/common/index.js';
+import common from './src/routers/common/index.js';
 import AI from './src/routers/Ai/Llama.js';
 import admin from './src/routers/admin/index.js';
 import toutiaoHot from './src/routers/toutiaoHot/index.js';
-import authRoute from './src/routers/auth/index.js';
-import userRoute from './src/routers/user/index.js';
-import { USERS, STATICDATAS,} from './src/models/index.js'
+import auth from './src/routers/auth/index.js';
+import user from './src/routers/user/index.js';
 import bing from './src/routers/bing/index.js'
+import upload from './src/routers/upload/index.js'
+import {connect} from './src/models/index.js'
 
 const app = express()
-const PORT = 4000;
-const upload = multer({ dest: 'upload' }).single('file', 'text')
-const uploadVideo = multer({ dest: 'uploadvideo' }).single('video', 'text')
-const uploadHeadImg = multer({ dest: 'uploadheadimg' }).single('headimg', 'text')
+connect()
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "http://localhost:3000" } });
-
 
 app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:5173'] }))
 app.use(express.json());
@@ -35,100 +29,28 @@ app.use(express.static('uploadheadimg'))
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }))
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb://localhost:27017').catch(error => handleError(error))
-mongoose.connection.on('error', error => { console.log(error); })
-mongoose.connection.once('connected', () => {
-    console.log('mongodb is connected');
-})
-
-
 // 监听客户端连接
 socketio(io)
-// common API
+// bing API
 app.use(bing)
-//admin
+//admin API
 app.use(admin)
-//ai
+//ai API
 app.use(AI)
-//toutiaoHot
+//toutiaoHot API
 app.use(toutiaoHot)
 //open api
-app.use(commonRoute)
-//auth API
+app.use(common)
+//auth API 上面的可以不适用jwt
 app.use(authMiddleware)
-////auth API
-app.use(authRoute)
-app.use(userRoute)
-app.post('/upload/:id', upload, async (req, res) => {
-  try {
-        const { targetId, staticType } = Object.assign({}, req.body)
-        const userid = req.params.id
-        const uuid = crypto.randomUUID()
-        const filepath = uuid + "_" + (req.file.originalname)
-        fs.renameSync(req.file.path, `upload/${filepath}`);
-        const data = new STATICDATAS({
-            staticType: staticType,
-            targetId: new mongoose.Types.ObjectId(targetId),
-            staticUrl: `http://127.0.0.1:4000/${filepath}`,
-            userId: new mongoose.Types.ObjectId(userid)
-        }
-        )
-        const datatosave = await data.save();
-        res.status(200).send(datatosave)
-    } catch (error) {
-        res.status(500).json({ massage: error.massage })
-    } 
-})
-app.post('/uploadvideo/:id', uploadVideo, async (req, res) => {
-    try {
-        const { targetId, staticType } = Object.assign({}, req.body)
-        const userid = req.params.id
-        const uuid = crypto.randomUUID()
-        const filepath = uuid + "_" + (req.file.originalname)
-        fs.renameSync(req.file.path, `uploadvideo/${filepath}`);
-        const data = new STATICDATAS({
-            staticType: staticType,
-            targetId: new mongoose.Types.ObjectId(targetId),
-            staticUrl: `http://127.0.0.1:4000/${filepath}`,
-            userId: new mongoose.Types.ObjectId(userid)
-        }
-        )
-        const datatosave = await data.save();
-        res.status(200).send(datatosave)
-    } catch (error) {
-        res.status(500).json({ massage: error.massage })
-    }
-})
-//上传用户头像
-app.post('/uploadheadimg/:id', uploadHeadImg, async (req, res) => {
-    try {
-        const { targetId, staticType } = Object.assign({}, req.body)
-        const userid = req.params.id
-        const userAndemail = await USERS.findOne({ _id: userid });//false
-        if (!userAndemail) { return res.status(400).json({ meassge: '找不到账号', status: false }) }
-        const uuid = crypto.randomUUID()
-        const filepath = uuid + "_" + (req.file.originalname)
-        fs.renameSync(req.file.path, `uploadheadimg/${filepath}`);
-        const data = new STATICDATAS({
-            staticType: staticType,
-            targetId: new mongoose.Types.ObjectId(targetId),
-            staticUrl: `http://127.0.0.1:4000/${filepath}`,
-            userId: new mongoose.Types.ObjectId(userid)
-        }
-        )
-        await data.save();
-        const datas = await USERS.findOneAndUpdate(
-            { _id: userid },
-            { headimg: `http://127.0.0.1:4000/${filepath}` },
-            { new: true, useFindAndModify: false } // 确保返回更新后的文档，并使用新的 findOneAndUpdate 行为
-        );
-        res.status(200).send(datas.headimg)
-    } catch (error) {
-        res.status(500).json({ massage: error.massage })
-    }
-})
-
-server.listen(PORT, () => {
+//auth API
+app.use(auth)
+//user API
+app.use(user)
+//upload API
+app.use(upload)
+//启动服务器
+server.listen(4000, () => {
     console.log(
         'server is open'
     )
