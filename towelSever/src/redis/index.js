cff2a8a8216e96de75e86b168ad6765746aadf3b
GeKaixing <x2890901420@Gmail.com>
2024-11-27 01:25:35 +0800

    fix(towelServer):修复redis连接不上的问题

--- towelSever/src/redis/index.js
+++ towelSever/src/redis/index.js
@@ -1,4 +1,4 @@
-import {creatClient}from 'redis';
-const redisClient=creatClient();
+import {createClient}from 'redis';
+const redisClient=createClient();
 redisClient.connect().catch((err) => console.error('Redis connection error:', err));
 export default redisClient;
