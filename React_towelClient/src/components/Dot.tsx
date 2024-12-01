b084f335e257bc5239304bddb68ae99a2e188952
GeKaixing <x2890901420@Gmail.com>
2024-12-02 01:22:23 +0800

    fix(React_towel):调整dot位置

--- React_towelClient/src/components/Dot.tsx
+++ React_towelClient/src/components/Dot.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function Dot() {
+export default function Dot({className=''}) {
   return (
-    <div className='w-2 h-2 rounded-full bg-[--hostColor]'></div>
+    <div className={`${className} w-2 h-2 rounded-full bg-[--hostColor]`}></div>
   )
 }
--- React_towelClient/src/components/Premium.tsx
+++ React_towelClient/src/components/Premium.tsx
@@ -39,7 +39,7 @@ export default function Premium({ setShowPremium }) {
                 <div className='flex space-x-10'>
                     {/* border-[--hostColor] border-solid border-2 rounded-my-rounded-10px */}
                     <section onClick={checkhandler} data-name='Premium' className='flex flex-col items-center w-full space-y-2'>
-                        <div className='flex items-center space-x-2'><p>Premium</p>{isDot === 'Premium' && <Dot></Dot>}</div>
+                        <div className='flex items-center space-x-2 relative'><p>Premium</p>{isDot === 'Premium' && <Dot className={'absolute left-16'}></Dot>}</div>
                         <div>按月计费 <span className='font-bold text-2xl'>15</span>/月</div>
                         <div className='flex space-x-2 self-start'> <img src={check} className='w-4 h-4'></img>  <div>it fugiat et cillum est ut aliqua</div></div>
                         <div className='flex space-x-2 self-start'> <img src={check} className='w-4 h-4'></img>  <div>it fugiat et cillum est ut aliqua</div></div>
@@ -48,7 +48,7 @@ export default function Premium({ setShowPremium }) {
                         <div className='border-[--hostColor] border-solid border-4 rounded-my-rounded-10px text-2xl' onClick={() => buyHandler(400)}>购买</div>
                     </section>
                     <section onClick={checkhandler} data-name='Premium+' className={`flex flex-col items-center w-full space-y-2`}>
-                        <div className='flex items-center space-x-2'><p>Premium+</p>{isDot === 'Premium+' && <Dot></Dot>}</div>
+                        <div className='flex items-center space-x-2 relative'><p>Premium+</p>{isDot === 'Premium+' && <Dot className='absolute left-[4.6rem]'></Dot>}</div>
                         <div>按月计费 <span className='font-bold text-2xl'>20</span>/月</div>
                         <div className='flex space-x-2 self-start'> <img src={check} className='w-4 h-4'></img>  <div>it fugiat et cillum est ut aliqua</div></div>
                         <div className='flex space-x-2 self-start'> <img src={check} className='w-4 h-4'></img>  <div>it fugiat et cillum est ut aliqua</div></div>
@@ -65,7 +65,7 @@ export default function Premium({ setShowPremium }) {
                 <p className='w-96 h-22 text-xs'>订阅即表示你同意我们的 购买者服务条款。如条款中所述，除非提前取消，否则订阅服务将自动续订。随时取消. 请在续订前至少 24 小时取消订阅，以免产生额外费用。订阅需提供一个经认证的手机号码。如果你在其他平台上进行了订阅，请通过该平台来管理相关订阅服务。</p>
             </div>
             {clientSecret && show && <Stripe clientSecret={clientSecret}></Stripe>}
-        </Portal>
+ ···       </Portal>
     )
 }
 Premium.propTypes = {
