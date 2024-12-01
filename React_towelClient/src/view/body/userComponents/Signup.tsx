6ddae1ade264e94635790affe1e144c0bca03d46
GeKaixing <x2890901420@Gmail.com>
2024-12-02 01:31:15 +0800

    fix(React_towel):修改登录

--- React_towelClient/src/view/body/userComponents/Signup.tsx
+++ React_towelClient/src/view/body/userComponents/Signup.tsx
@@ -67,15 +67,15 @@ export default function Signup() {
         <img src={process.env.PUBLIC_URL + '/logo.png'} ></img>
       </div>
       <form className='flex flex-col justify-center items-center ' onSubmit={(e) => { e.preventDefault(); }}>
-        <input required className='w-36 h-6 mt-2.5 bg-[--boxColor] border-0 text-[--fontColor]' type='text' value={valuename_signup} onChange={(e) => { e.preventDefault(); setValuename_signup(e.target.value) }} placeholder={t('name')} >
+        <input required className='w-60 h-6 mt-2.5 bg-[--boxColor] border-0 text-[--fontColor]' type='text' value={valuename_signup} onChange={(e) => { e.preventDefault(); setValuename_signup(e.target.value) }} placeholder={t('name')} >
         </input>
-        <input required type='password' className='w-36 h-6 mt-2.5 bg-[--boxColor] border-0 text-[--fontColor]' value={valuepassword_signup} onChange={(e) => { e.preventDefault(); setValuepassword_signup(e.target.value) }} placeholder={t('password')}>
+        <input required type='password' className='w-60 h-6 mt-2.5 bg-[--boxColor] border-0 text-[--fontColor]' value={valuepassword_signup} onChange={(e) => { e.preventDefault(); setValuepassword_signup(e.target.value) }} placeholder={t('password')}>
         </input>
-        <input required type='email' className='w-36 h-6 mt-2.5 bg-[--boxColor] border-0 text-[--fontColor]' value={inputEmailData} onChange={validateEmail} placeholder={t('EmailTheVerificationCode')}>
+        <input required type='email' className='w-60 h-6 mt-2.5 bg-[--boxColor] border-0 text-[--fontColor]' value={inputEmailData} onChange={validateEmail} placeholder={t('EmailTheVerificationCode')}>
         </input>
         {isEmail ?
           <>
-            <input required type='text' minLength={5} maxLength={5} className='w-36 h-6 mt-2.5 bg-[--boxColor] border-0 text-[--fontColor]' value={verificationcode} onChange={(e) => { e.preventDefault(); setVerificationCode(e.target.value) }} placeholder={t('Verification')}>
+            <input required type='text' minLength={5} maxLength={5} className='w-60 h-6 mt-2.5 bg-[--boxColor] border-0 text-[--fontColor]' value={verificationcode} onChange={(e) => { e.preventDefault(); setVerificationCode(e.target.value) }} placeholder={t('Verification')}>
             </input>
             <button className='mt-2.5 w-24 h-6 bg-[--boxColor] border-0 text-[--fontColor]' onClick={e => { e.preventDefault; sendEmailRegister() }}>
             {t('SendVerificationCode')}
