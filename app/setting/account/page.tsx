8863b25254679774128df9a00e6ba5f48c3b60a5
GeKaixing <x2890901420@Gmail.com>
2025-03-07 22:20:47 +0800

    refactor(account): 简化用户信息展示结构，提升可读性

--- app/setting/account/page.tsx
+++ app/setting/account/page.tsx
@@ -17,14 +17,12 @@ export default async function Page() {
         <div>
             <h1>用户信息</h1>
             {/* 将数据传递给客户端组件 */}
-            <div>
-                <p>用户名：{res?.username}</p>
-                <p>邮箱：{res?.email}</p>
-                <p>创建时间：{res?.createDate}</p>
-                <p>头像：{res?.headimg}</p>
-                <p>电话：{res?.phoneNumber}</p>
-                <p>生日：{res?.birthday || '还没有设置'}</p>
-            </div>
+            <p>用户名：{res?.username}</p>
+            <p>邮箱：{res?.email}</p>
+            <p>创建时间：{res?.createDate}</p>
+            <p>头像：{res?.headimg}</p>
+            <p>电话：{res?.phoneNumber}</p>
+            <p>生日：{res?.birthday || '还没有设置'}</p>
             <OutButton></OutButton>
         </div>
     );
