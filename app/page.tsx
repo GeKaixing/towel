e83221a32dabb24523155da9086002a83841b39b
GeKaixing <x2890901420@Gmail.com>
2025-03-12 23:23:13 +0800

    feat(home): integrate user authentication in MoreButton and enhance post display with cookie support

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import testImg from "@/assets/test.png";
 import Link from "next/link";
 import { CommentIcon, LikeIcon, ShareIcon, StarIcon } from "@/components/icon";
 import MoreButton from "@/components/MoreButton";
+import { getCookie } from "@/util/getcookie";
 
 interface Post {
   _id: string;
@@ -17,54 +18,69 @@ interface Post {
   postFavorite: number;
 }
 export default async function Home() {
-  const posts = await GET()
+  const posts = await GET();
+  const cookie = await getCookie();
   return (
     <div className="space-y-2">
       {posts.map((item) => (
-        <div key={item.postCreateDate} className="w-full border-2 dark:hover:border-[#353535] dark:border-[#0a0a0a]  border-gray-100 hover:border-gray-200 p-2 rounded-xl ">
-          <Link href={`/post/${item._id}`}  >
-            <header className="flex justify-between items-center gap-2">
-              <div className="flex flex-row gap-2 items-center">
-                <Image width={30} height={30} src={testImg} alt="react logo"></Image>
-                <div className="font-bold">{item.user.username}</div>
-              </div>
-              <MoreButton></MoreButton>
-            </header>
+        <div
+          key={item.postCreateDate}
+          className="w-full border-2 dark:hover:border-[#353535] dark:border-[#0a0a0a]  border-gray-100 hover:border-gray-200 p-2 rounded-xl "
+        >
+          <header className="flex justify-between items-center gap-2">
+            <div className="flex flex-row gap-2 items-center">
+              <Image
+                width={30}
+                height={30}
+                src={testImg}
+                alt="react logo"
+              ></Image>
+              <div className="font-bold">{item.user.username}</div>
+            </div>
+              <MoreButton userId={cookie?.userid} postUserid={item.postUserId}></MoreButton>
+          </header>
+          <Link href={`/post/${item._id}`}>
             <main className="mt-2 mb-2 ">
               <div className="font-bold ">{item.postText}</div>
             </main>
-            <footer className="flex justify-between">
-              <div className="flex items-center gap-2">
-                <LikeIcon></LikeIcon>
-                <span className=" text-gray-400 hover:text-gray-800">{item.postLike}</span>
-              </div>
-              <div className="flex items-center gap-2">
-                <CommentIcon></CommentIcon>
-                <span className=" text-gray-400 hover:text-gray-800">{item.postComment}</span>
-              </div>
-              <div className="flex items-center gap-2">
-                <StarIcon></StarIcon>
-                <span className=" text-gray-400 hover:text-gray-800">{item.postFavorite}</span>
-              </div>
-              <div className="flex items-center gap-2">
-                <ShareIcon></ShareIcon>
-              </div>
-            </footer>
           </Link>
+          <footer className="flex justify-between">
+            <div className="flex items-center gap-2">
+              <LikeIcon></LikeIcon>
+              <span className=" text-gray-400 hover:text-gray-800">
+                {item.postLike}
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <CommentIcon></CommentIcon>
+              <span className=" text-gray-400 hover:text-gray-800">
+                {item.postComment}
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <StarIcon></StarIcon>
+              <span className=" text-gray-400 hover:text-gray-800">
+                {item.postFavorite}
+              </span>
+            </div>
+            <div className="flex items-center gap-2">
+              <ShareIcon></ShareIcon>
+            </div>
+          </footer>
         </div>
       ))}
-    </div >
+    </div>
   );
 }
 async function GET(): Promise<Post[]> {
   const res = await fetch(`${process.env.ORIGIN}/api/post`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     next: {
-      tags: ['post'],
+      tags: ["post"],
     },
-  })
-  return await res.json()
+  });
+  return await res.json();
 }
--- components/MoreButton.tsx
+++ components/MoreButton.tsx
@@ -1,14 +1,44 @@
-'use client'
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
-export default function MoreButton({...props}) {
+export default function MoreButton({
+  userId,
+  postUserid,
+  ...props
+}: {
+  userId: string;
+  postUserid: string;
+}) {
+  console.log(userId, postUserid);
+  const [show, setShow] = useState(false);
   return (
-    <div {...props} onClickCapture={(event) => {
-      event.stopPropagation();
-      event.preventDefault();
-      console.log('more');
-    }}>
-      ...
+    <div className="relative cursor-pointer">
+      {show && (
+        <div
+          className=" absolute  right-4 top-4 w-auto h-auto
+          flex flex-col gap-2 flex-nowrap 
+          "
+        >
+          {userId === postUserid && (
+            <div className=" whitespace-nowrap overflow-hidden text-ellipsis cursor-pointer">
+              删除
+            </div>
+          )}
+          <div className="whitespace-nowrap overflow-hidden text-ellipsis cursor-pointer">
+            举报
+          </div>
+        </div>
+      )}
+      <div
+        {...props}
+        onClickCapture={(event) => {
+          event.stopPropagation();
+          event.preventDefault();
+          setShow(!show);
+        }}
+      >
+        ...
+      </div>
     </div>
-  )
+  );
 }
