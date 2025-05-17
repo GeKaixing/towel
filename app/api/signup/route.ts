6ded9ce316ca0a3132d52246f445d3f7f2e64629
GeKaixing <x2890901420@Gmail.com>
2025-05-17 22:46:46 +0800

    feat(signup): enhance registration logic with verification code validation

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,23 +1,29 @@
 import bcrypt from "bcrypt";
-import { USERS } from "@/models/db";
+import { USERS, verificationCodes } from "@/models/db";
 import { NextResponse } from "next/server";
-import redisClient from "@/redis";
 
-export async function POST(request: Request) { 
- try {
-    const { username, password, email} = await request.json();
+export async function POST(request: Request) {
+  try {
+    const { username, password, email } = await request.json();
     const userAndemail = await USERS.findOne({
       $or: [{ username }, { email }],
-    }); 
-    const rediskey=`nodemailerRegister:${email}`;
-    const user= await redisClient.get(`nodemailerRegister:${email}`);
-    if (!(userAndemail || user)) {
-        return NextResponse.json(
-            { message: "验证码错误" },
-            { status: 401 }
-          );
+    });
+
+    const d = await verificationCodes.findOne({
+      email: email,
+      purpose: "account-registration",
+    });
+    if (!d) {
+      return NextResponse.json({ message: "验证码错误" }, { status: 401 });
     }
-    await redisClient.del(rediskey);
+    await verificationCodes.findOneAndDelete({
+      email: email,
+      purpose: "account-registration",
+    });
+
+    if (userAndemail) {
+      return NextResponse.json({ message: "账号重复" }, { status: 401 });
+    }
     const hashpassword = await bcrypt.hash(password, 10);
     const userdata = new USERS({
       username,
@@ -27,14 +33,8 @@ export async function POST(request: Request) {
       premium: false,
     });
     await userdata.save();
-    return NextResponse.json(
-        { message: "注册成功" },
-        { status: 201 }
-      );
+    return NextResponse.json({ message: "注册成功" }, { status: 201 });
   } catch {
-    return NextResponse.json(
-        { message: "服务器错误" },
-        { status: 504 }
-      );
+    return NextResponse.json({ message: "服务器错误" }, { status: 504 });
   }
 }
